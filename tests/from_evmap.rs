7179efa40b95f9269fbc48eac288b9ff9595cf87
Joshua Lockerman <j@Js-MacBook-Air.home>
2017-08-21 12:49:12 -0400

    add tests from ev-map and licenses.

---
+++ tests/from_evmap.rs
@@ -0,0 +1,62 @@
+// from https://github.com/jonhoo/rust-evmap/blob/440a5850ffb528281d1dafe5175ad0b67cf4348c/tests/lib.rs
+
+extern crate click_map;
+
+use click_map::HashMap;
+
+#[test]
+fn busybusybusy_fast() {
+    busybusybusy_inner(false);
+}
+#[test]
+fn busybusybusy_slow() {
+    busybusybusy_inner(true);
+}
+
+fn busybusybusy_inner(slow: bool) {
+    use std::time;
+    use std::thread;
+
+    let threads = 4;
+    let mut n = 1000;
+    if !slow {
+        n *= 100;
+    }
+    let map = HashMap::new();
+
+    let rs: Vec<_> = (0..threads)
+        .map(|_| {
+            let map = map.clone();
+            thread::spawn(move || {
+                // rustfmt
+                for i in 0..n {
+                    let i = i.into();
+                    loop {
+                        match map.get_and(&i, |rs| {
+                            if slow {
+                                thread::sleep(time::Duration::from_millis(2));
+                            }
+                            *rs
+                        }) {
+                            Some(rs) => {
+                                assert_eq!(rs, i);
+                                break;
+                            }
+                            None => {
+                                thread::yield_now();
+                            }
+                        }
+                    }
+                }
+            })
+        })
+        .collect();
+
+    for i in 0..n {
+        map.insert(i, i);
+    }
+
+    for r in rs {
+        r.join().unwrap();
+    }
+}
